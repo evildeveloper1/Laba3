25d9748eafa8d6e4b785a758a1cecc2ef7b8e720
{{evildeveloper1}} <{{valerasarkisan285@gmail.com}}>
2023-12-04 21:20:20 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+/*
+* class Point
+*/
+class Point
+{
+    public double X { get; }
+    public double Y { get; }
+    public string Name { get; }
+
+    public Point(double x, double y, string name)
+    {
+        X = x;
+        Y = y;
+        Name = name;
+    }
+}
+/*
+* class Figure
+*/
+class Figure
+{
+    private Point[] points;
+
+    public Figure(params Point[] points)
+    {
+        if (points.Length < 3 || points.Length > 5)
+        {
+            throw new ArgumentException("A figure must have 3 to 5 points.");
+        }
+        this.points = points;
+    }
+
+    public double GetSideLength(Point A, Point B)
+    {
+        return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+    }
+
+    public void CalculatePerimeter()
+    {
+        double perimeter = 0;
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            perimeter += GetSideLength(points[i], points[i + 1]);
+        }
+        perimeter += GetSideLength(points[points.Length - 1], points[0]);
+
+        Console.WriteLine($"Perimeter of the figure: {perimeter}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Point p1 = new Point(0, 0, "A");
+        Point p2 = new Point(0, 4, "B");
+        Point p3 = new Point(3, 0, "C");
+
+        Figure figure = new Figure(p1, p2, p3);
+        figure.CalculatePerimeter();
+    }
+}
---
+++ Laba3/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+* class Rectangle
+*/
+class Rectangle
+{
+    private double side1;
+    private double side2;
+
+    public Rectangle(double side1, double side2)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+
+    public double CalculateArea()
+    {
+        return side1 * side2;
+    }
+
+    public double CalculatePerimeter()
+    {
+        return 2 * (side1 + side2);
+    }
+
+    public double Area
+    {
+        get { return CalculateArea(); }
+    }
+
+    public double Perimeter
+    {
+        get { return CalculatePerimeter(); }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the length of the first side of the rectangle:");
+        double side1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the length of the second side of the rectangle:");
+        double side2 = Convert.ToDouble(Console.ReadLine());
+
+        Rectangle rect = new Rectangle(side1, side2);
+
+        Console.WriteLine($"Area of the rectangle: {rect.Area}");
+        Console.WriteLine($"Perimeter of the rectangle: {rect.Perimeter}");
+    }
+}
